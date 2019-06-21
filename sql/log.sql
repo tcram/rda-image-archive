621fbabc54395fd1fedd1f4133a9e961f19cc39a
Colton Grainger <colton.grainger@colorado.edu>
2019-06-20 18:02:05 -0600

    Create virtual columns for hex_id and ut1_start_date_time

--- sql/log.sql
+++ sql/log.sql
@@ -1,11 +1,10 @@
 create table log 
 (
-    log_id unsigned smallint primary key auto_increment,
+    log_id unsigned smallint auto_increment primary key,
     ship_id unsigned smallint,
-    number_of_daily_observations int(2) default 24,
-    /* needs a "transcribed" boolean field */
-    /* ltemplate enum(...) */
-    /* lregion varchar(40), */
+    /* observations_per_page int(2) default 24 comment '(0--99) Number of meteorological observations per page.', */
+    /* template enum('') comment 'Template for logbook.', */
+    /* marine_regions varchar(1000) comment 'Comma separated list of marine region names (fuzzy). Can include seas, sandbanks, seamounts, ridges, bays, sampling stations, or ports.' */
     foreign key (ship_id) references ship(ship_id) on delete restrict
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- sql/page.sql
+++ sql/page.sql
@@ -1,13 +1,18 @@
 create table page 
 (
-    page_id binary(16) not null primary key,
-    local_start_date date comment 'Page start date in local time. Format: YYYY-MM-DD'
-    local_timezone varchar(6) comment 'Offset from UTC to the local time, e.g. for "15:00−03:30" do 15:00 − (−03:30) to get 18:30 UTC. Format: "HH:MM" or "-HH:MM"'
-
-    /* pid_hex varchar(32) generated always as hex(pid_bin) virtual, # TODO needs extension */
-    /* psize bigint, */
+    page_id binary(16) primary key comment '16-byte representation of 32-char hexidecimal page UUID. Generated during image file import (by removing hyphens from 36-char UUID).',
+    page_id_hex char(32) generated always as (hex(page_id)) virtual,
+    /* "MySQL will automatically convert a date or time to its numerical equivalent when it is used in a numerical context; and it will do the reverse as well." (Dyer, 2005) */
+    local_start_date date default null comment 'Format: "YYYY-MM-DD", or, numerically, YYYYMMDD. Local date at page start.',
+    local_start_time time default "00:00:00" comment 'Format: "HH:MM:SS", or, numerically, HHMMSS. Local time at page start. Should be entered as a postive value between "00:00" (or 000000) and "23:59" (or 235900).',
+    local_time_zone time default null comment 'Format: should be entered as a signed value between "-12:00" (or -120000) and "12:00" (or 120000). The local timezone at page start is defined to be the (signed) hours and minutes from UT1 solar time to local time. For example, in timezone -03:30, the local time 15:00 refers to the UT1 time 18:30.',
+    ut1_start_datetime datetime generated always as (date_sub(local_start_date, interval local_time_zone hour_second))
+    /* TODO psize bigint, */
     /* lid int not null, */
     /* pformat varchar(10), */
     /* porder int not null */
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into page (page_id, local_start_date, local_start_time, local_time_zone) values ("snarf", "1850-01-05", "00:00", "-03:30" );
+
